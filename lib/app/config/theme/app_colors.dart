a272fd69d89216c2d8163889f01f0ea15c8e65df
pedrohsampaioo <pedrohsampaioo@outlook.com>
2021-08-16 22:07:56 -0300

    feat: create app colors

---
+++ lib/app/config/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static Color get white => Colors.white;
+  static Color get black => Color(0xFF006CFF);
+  static Color get purple => Color(0xFFB678FF);
+  static Color get yellow => Color(0xFFFFE761);
+  static Color get red => Color(0xFFF45E6D);
+  static Color get green => Color(0xFF61DEA4);
+  static Color get gray => Color(0xFFEBEFF5);
+}
