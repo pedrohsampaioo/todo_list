ef771248b27a7d871b75c9099a4c4c7dce7009f5
pedrohsampaioo <pedrohsampaioo@outlook.com>
2021-08-18 16:47:01 -0300

    feat: create category widget

---
+++ lib/app/features/home/widgets/category_widget.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class CategoryWidgeet extends StatelessWidget {
+  final Color backgroundColor;
+  final Color titleColor;
+  final Color subtitleColor;
+  final String title;
+  final int tasksAmount;
+
+  const CategoryWidgeet({
+    Key? key,
+    required this.backgroundColor,
+    required this.titleColor,
+    required this.subtitleColor,
+    required this.title,
+    required this.tasksAmount,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            title,
+            textAlign: TextAlign.start,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style:
+                Theme.of(context).textTheme.headline6!.apply(color: titleColor),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            '$tasksAmount ${tasksAmount > 1 ? 'tasks' : 'task'}',
+            textAlign: TextAlign.start,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context)
+                .textTheme
+                .headline6!
+                .apply(color: subtitleColor),
+          ),
+        ],
+      ),
+    );
+  }
+}
