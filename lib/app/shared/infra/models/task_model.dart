e8fc256c2d1456ad4bc45acfb8663a2c3fce1c4c
pedrohsampaioo <pedrohsampaioo@outlook.com>
2021-08-23 21:17:30 -0300

    feat: create task model

---
+++ lib/app/shared/infra/models/task_model.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'task_model.g.dart';
+
+@JsonSerializable()
+class TaskModel {
+  final String id;
+  final bool isCompleted;
+  final String title;
+  @JsonKey(fromJson: _fromJsonColor, toJson: _toJsonColor)
+  final Color categoryColor;
+
+  const TaskModel({
+    required this.id,
+    required this.isCompleted,
+    required this.title,
+    required this.categoryColor,
+  });
+
+  factory TaskModel.fromJson(Map<String, dynamic> json) =>
+      _$TaskModelFromJson(json);
+  Map<String, dynamic> toJson() => _$TaskModelToJson(this);
+
+  static Color _fromJsonColor(Map<String, dynamic> json) =>
+      Color(json['categoryColor'] as int);
+  static int _toJsonColor(Color categoryColor) => categoryColor.value;
+
+  TaskModel copyWith({
+    String? id,
+    bool? isCompleted,
+    String? title,
+    Color? categoryColor,
+  }) {
+    return TaskModel(
+      id: id ?? this.id,
+      isCompleted: isCompleted ?? this.isCompleted,
+      title: title ?? this.title,
+      categoryColor: categoryColor ?? this.categoryColor,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is TaskModel && other.id == id;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        isCompleted.hashCode ^
+        title.hashCode ^
+        categoryColor.hashCode;
+  }
+}
---
+++ lib/app/shared/infra/models/task_model.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'task_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+TaskModel _$TaskModelFromJson(Map<String, dynamic> json) => TaskModel(
+      id: json['id'] as String,
+      isCompleted: json['isCompleted'] as bool,
+      title: json['title'] as String,
+      categoryColor: TaskModel._fromJsonColor(
+          json['categoryColor'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$TaskModelToJson(TaskModel instance) => <String, dynamic>{
+      'id': instance.id,
+      'isCompleted': instance.isCompleted,
+      'title': instance.title,
+      'categoryColor': TaskModel._toJsonColor(instance.categoryColor),
+    };
