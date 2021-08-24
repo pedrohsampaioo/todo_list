5ae5cfab9bd6f85c7dbc36597a37c22fc8a9b3b8
pedrohsampaioo <pedrohsampaioo@outlook.com>
2021-08-23 21:18:10 -0300

    feat: create category repository

---
+++ lib/app/shared/infra/failure/update_category_failure.dart
@@ -0,0 +1,1 @@
+enum UpdateCategoryFailure { uncreatedCategory }
---
+++ lib/app/shared/infra/repositories/category_repository.dart
@@ -0,0 +1,61 @@
+import 'package:dartz/dartz.dart';
+import 'package:sembast/sembast.dart';
+
+import 'package:todo_list/app/config/categories/category_theme.dart';
+import 'package:todo_list/app/shared/infra/failure/update_category_failure.dart';
+import 'package:todo_list/app/shared/infra/models/category_model.dart';
+import 'package:todo_list/app/shared/infra/models/task_model.dart';
+
+class CategoryRepository {
+  final Database _database;
+  final StoreRef<String, Map<String, dynamic>> _store;
+
+  const CategoryRepository({
+    required Database database,
+    required StoreRef<String, Map<String, dynamic>> store,
+  })  : _database = database,
+        _store = store;
+
+  Future<CategoryModel> createCategory({
+    required String id,
+    required String title,
+    required CategoryThemeVariations categoryTheme,
+  }) async {
+    final category = CategoryModel(
+      categoryTheme: categoryTheme,
+      title: title,
+      id: id,
+      tasks: [],
+    );
+    await _store.record(id).put(_database, category.toJson());
+    return category;
+  }
+
+  Future<Either<UpdateCategoryFailure, CategoryModel>> updateCategory({
+    required String id,
+    String? title,
+    List<TaskModel>? tasks,
+    CategoryThemeVariations? categoryTheme,
+  }) async {
+    final categoryToUpdate = await _store.record(id).get(_database);
+    if (categoryToUpdate == null) {
+      return left(UpdateCategoryFailure.uncreatedCategory);
+    }
+    final updatedCategory = CategoryModel.fromJson(categoryToUpdate).copyWith(
+      id: id,
+      title: title,
+      tasks: tasks,
+      categoryTheme: categoryTheme,
+    );
+    await _store.record(id).put(_database, updatedCategory.toJson());
+    return right(updatedCategory);
+  }
+
+  Future<void> deleteCategory({required String id}) async {
+    await _store.record(id).delete(_database);
+  }
+
+  Stream<CategoryModel> getAllCategoriesAsStream() =>
+      _store.stream(_database).map<CategoryModel>(
+          (recordSnapshot) => CategoryModel.fromJson(recordSnapshot.value));
+}
